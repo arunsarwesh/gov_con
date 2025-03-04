c5f2e4f121bb724b7f01efeec15b9522867f1205
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2025-03-04 11:29:33 +0530

    Update binary cache files and add a blank line in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ocr&)qgrmz+4e6+&%1%h6wdw9$ert45kw#94(-bn^+zsb2elc#'
 
